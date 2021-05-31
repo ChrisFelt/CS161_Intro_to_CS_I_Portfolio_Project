e0308acea3d907b60130bdc38850d8a918a5df0c
ChrisFelt <christopher.f.felt@gmail.com>
2021-05-31 14:46:02 -0400

    vertical and horizontal movement implemented __subvert_piece method implemented __check_win method implemented initial tests all OK

--- OrthokonBoard.py
+++ OrthokonBoard.py
@@ -6,10 +6,10 @@ class OrthokonBoard:
         # define initial board
         # list of rows in each column, column/row number corresponds to list number
         # R = red, E = empty, Y = yellow
-        self._board = [["R", "E", "E", "Y"], #  column 0
-                       ["R", "E", "E", "Y"], #  column 1
-                       ["R", "E", "E", "Y"], #  column 2
-                       ["R", "E", "E", "Y"]] #  column 3
+        self._board = [["R", "E", "E", "Y"],  # column 0
+                       ["R", "E", "E", "R"],  # column 1
+                       ["R", "E", "E", "R"],  # column 2
+                       ["R", "E", "E", "R"]]  # column 3
         self._current_state = "UNFINISHED"
 
     # get method returning current game state
@@ -42,14 +42,14 @@ class OrthokonBoard:
     def __move_down(self, from_col, from_row, to_row):
 
         # first check if any pieces are in the way
-        for i in range(abs(from_row - to_row) - 1):
+        for i in range(abs(from_row - to_row)):
 
             if self._board[from_col][from_row + (i + 1)] != "E":
 
                 return False
 
         # loop for number of spaces moved
-        for i in range(abs(from_row - to_row) - 1):
+        for i in range(abs(from_row - to_row)):
 
             # check if new space is empty
             if self._board[from_col][from_row + (i + 1)] == "E":
@@ -92,17 +92,61 @@ class OrthokonBoard:
                 self._board[from_col - i][from_row] = "E"
 
     # method that flips pieces that are orthogonally adjacent after a move
-    def _subvert_piece(self, col, row):
+    def __subvert_piece(self, col, row):
 
+        # check spaces in adjacent rows
+        for i in range(-1, 2):
 
+            # check if opposing piece present
+            if self._board[col][row + i] != "E" and self._board[col][row + i] != self._board[col][row]:
 
+                # flip the piece!
+                self._board[col][row + i] = self._board[col][row]
 
+        # check spaces in adjacent columns
+        for i in range(-1, 2):
+
+            # check if opposing piece present
+            if self._board[col + i][row] != "E" and self._board[col + i][row] != self._board[col][row]:
+
+                # flip the piece!
+                self._board[col + i][row] = self._board[col][row]
+
+    # method for checking the state of the board and declaring a winner
+    def __check_win(self):
+
+        # check if no red pieces remain
+        if "R" not in self._board[0] and "R" not in self._board[1] and \
+                "R" not in self._board[2] and "R" not in self._board[3]:
+
+            # yellow wins
+            self._current_state = "YELLOW WON"
+            return True
+
+        # check if no yellow pieces remain
+        elif "Y" not in self._board[0] and "Y" not in self._board[1] and \
+                "Y" not in self._board[2] and "Y" not in self._board[3]:
+
+            # red wins
+            self._current_state = "RED WON"
+            return True
+
+        else:  # not necessary?
+
+            return False  # no winner yet
+
+    # method for handling piece movement on the board
     def make_move(self, from_col, from_row, to_col, to_row):
 
         # check if piece was moved to same location as origin
         if from_col == to_col and from_row == to_row:
 
-            return False # Failed to move!
+            return False  # Failed to move!
+
+        # check if destination is within the board
+        if from_col > 3 or from_row > 3 or to_col > 3 or to_row > 3:
+
+            return False  # Out of bounds!
 
         # check if the move is orthogonal
         if from_col == to_col or from_row == to_row:
@@ -113,14 +157,38 @@ class OrthokonBoard:
                 # check if moving up
                 if from_row > to_row:
 
-                    # move piece up using _move_up method
-                    self.__move_up(from_col, from_row, to_row)
+                    # check if move is legal using __move_up method
+                    # moves piece if True
+                    if self.__move_down(from_col, from_row, to_row):
+
+                        # flip opponent's adjacent pieces
+                        self.__subvert_piece(to_col, to_row)
+
+                        # check if this move resulted in a win condition
+                        self.__check_win()
+                        return True
+
+                    else:
+
+                        return False
 
                 # check if moving down
                 if from_row < to_row:
 
-                    # move piece down using _move_down method
-                    self.__move_down(from_col, from_row, to_row)
+                    # check if move is legal using __move_down method
+                    # moves piece if True
+                    if self.__move_down(from_col, from_row, to_row):
+
+                        # flip opponent's adjacent pieces
+                        self.__subvert_piece(to_col, to_row)
+
+                        # check if this move resulted in a win condition
+                        self.__check_win()
+                        return True
+
+                    else:
+
+                        return False
 
             # check if move is horizontal
             if from_row == to_row:
@@ -128,17 +196,47 @@ class OrthokonBoard:
                 # check if moving right
                 if from_col > to_col:
 
-                    # move piece right using __move_right method
-                    self.__move_right(from_col, from_row, to_col)
+                    # check if move is legal using __move_right method
+                    # moves piece if True
+                    if self.__move_right(from_col, from_row, to_col):
 
-            self._subvert_piece(to_col, to_row)
-            return True
+                        # flip opponent's adjacent pieces
+                        self.__subvert_piece(to_col, to_row)
+
+                        # check if this move resulted in a win condition
+                        self.__check_win()
+                        return True
+
+                    else:
+
+                        return False
+
+                # check if moving left
+                if from_col > to_col:
+
+                    # check if move is legal using __move_left method
+                    # moves piece if True
+                    if self.__move_left(from_col, from_row, to_col):
+
+                        # flip opponent's adjacent pieces
+                        self.__subvert_piece(to_col, to_row)
+
+                        # check if this move resulted in a win condition
+                        self.__check_win()
+                        return True
+
+                    else:
+
+                        return False
+
+            # return True
 
         # check if move is diagonal
-        elif abs(from_col - to_col) // abs(from_row - to_row) == 1:
+        # elif abs(from_col - to_col) // abs(from_row - to_row) == 1:
+
 
 board = OrthokonBoard()
-board.make_move(0, 3, 0, 0)  # The yellow player moves a piece diagonally, flipping one red piece to yellow
+print(board.make_move(0, 0, 0, 2))  # The yellow player moves a piece diagonally, flipping one red piece to yellow
 # board.make_move(0, 2, 2, 0)  # The red player moves a piece diagonally, flipping two yellow pieces to red
 
 board.print_board()
